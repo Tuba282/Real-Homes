8b585db50cc6be603526fda2f49f8257c994cf66
Tuba282 <tubajan282@gmail.com>
2025-09-28 02:28:17 +0500

    add ts.config.json

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 // Import the Leaflet icon override before any map components mount so
 // markers will use our public images instead of node_modules paths.
 import './leafletIcon'
-import App from './App.tsx'
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
