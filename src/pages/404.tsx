ca8bf1108d08e0e2a65924c7167295335a33f374
Tuba282 <tubajan282@gmail.com>
2025-09-28 00:23:34 +0500

    update dependencies

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,7 @@
 
 const NotFound = () => {
     return (
-        <div>NotFound</div>
+        <div className="w-full min-h-screen flex justify-center items-center text-2xl md:text-4xl text-blue-300 font-[borik]">NotFound</div>
     )
 }
 
